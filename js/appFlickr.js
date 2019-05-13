58980a0f4e1c53075e871d6d5063fd8078f6ff07
clara <clara@LAPTOP-ETKJ0DAS>
2019-05-13 17:47:03 +0200

    mostrar fotos usuario

--- js/appFlickr.js
+++ js/appFlickr.js
@@ -1,3 +1,5 @@
+let photosGuardado = new Map();
+
 $(function() {
 $.getJSON('https://api.flickr.com/services/rest/?&method=flickr.photos.search&api_key=' 
 	 + api_key + '&user_id=' +user_id + 
@@ -14,6 +16,21 @@ function mostrar_fotos(info){
 	   console.debug(url);
 	   $("#imagenes").append($("<img/>").attr("src",url));
     }
+}
+
+function mostrar_fotos_usuario() {
+    $('#imagenes').append("<div class=\"gif\"><img src=\"img/cargando-gif.gif\"></div>");
+    var criteriofinal1 = calculaCriterio();
+    $.getJSON('https://api.flickr.com/services/rest/?&method=flickr.photos.search&api_key=' +
+        api_key + '&user_id=' + user_id + criteriofinal1 +
+        '&format=json&nojsoncallback=1',
+        mostrar_fotos
+    )
+}
+
+function addPhotosGuardado(id) {
+    photosGuardado.set(id,photosBusqueda[Number(id)]);
+    actualizarFotos();
 }
 
 }
