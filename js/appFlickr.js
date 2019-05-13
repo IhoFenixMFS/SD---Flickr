532bb72b05f009470b90ab5449b01e1a977d752b
IhoFenixMFS <iho-ladamadeltiempo@live.com>
2019-05-13 20:27:28 +0200

    Pagina inicial con filtrado de todo y familiares y amigos

--- js/appFlickr.js
+++ js/appFlickr.js
@@ -1,37 +1,37 @@
-let photosGuardado = new Map();
-
 $(function() {
-$.getJSON('https://api.flickr.com/services/rest/?&method=flickr.photos.search&api_key=' 
-	 + api_key + '&user_id=' +user_id + 
-	'&format=json&nojsoncallback=1',
-	mostrar_fotos
-)
+	$.getJSON('https://api.flickr.com/services/rest/?&method=flickr.photos.getContactsPublicPhotos&api_key='
+							+ api_key
+							+ '&user_id='
+							+ user_id
+							+ '&format=json&nojsoncallback=1', mostrar_fotos)
 
-function mostrar_fotos(info){
+	
+
+})
+
+function filtrar_fotos() {
+	$.getJSON('https://api.flickr.com/services/rest/?&method=flickr.photos.getContactsPublicPhotos&api_key='
+					+ api_key + '&user_id=' + user_id + '&just_friends=1'
+					+ '&format=json&nojsoncallback=1', mostrar_fotos)
+}
+
+function mostrar_todo() {
+	$.getJSON('https://api.flickr.com/services/rest/?&method=flickr.photos.getContactsPublicPhotos&api_key='
+			+ api_key
+			+ '&user_id='
+			+ user_id
+			+ '&format=json&nojsoncallback=1', mostrar_fotos)
+}
+
+function mostrar_fotos(info) {
+	$("#imagenes").empty();
 	var i;
-	for (i=0;i<info.photos.photo.length;i++) {
-	   var item = info.photos.photo[i];
-	   var url = 'https://farm'+item.farm+".staticflickr.com/"+item.server
-		          +'/'+item.id+'_'+item.secret+'_m.jpg';
-	   console.debug(url);
-	   $("#imagenes").append($("<img/>").attr("src",url));
-    }
+	for (i = 0; i < info.photos.photo.length; i++) {
+		var item = info.photos.photo[i];
+		var url = 'https://farm' + item.farm + ".staticflickr.com/"
+				+ item.server + '/' + item.id + '_' + item.secret
+				+ '_m.jpg';
+		console.debug(url);
+		$("#imagenes").append($("<img/>").attr("src", url));
+	}
 }
-
-function mostrar_fotos_usuario() {
-    $('#imagenes').append("<div class=\"gif\"><img src=\"img/cargando-gif.gif\"></div>");
-    var criteriofinal1 = calculaCriterio();
-    $.getJSON('https://api.flickr.com/services/rest/?&method=flickr.photos.search&api_key=' +
-        api_key + '&user_id=' + user_id + criteriofinal1 +
-        '&format=json&nojsoncallback=1',
-        mostrar_fotos
-    )
-}
-
-function addPhotosGuardado(id) {
-    photosGuardado.set(id,photosBusqueda[Number(id)]);
-    actualizarFotos();
-}
-
-}
-)		
